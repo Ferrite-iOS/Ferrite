0ce54327fb589fe64da40db93cd3cdce582749e6
kingbri <bdashore3@gmail.com>
2022-07-01 12:45:04 -0400

    Initial Commit

---
+++ Torrenter/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Torrenter
+//
+//  Created by Brian Dashore on 7/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Torrenter/TorrenterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TorrenterApp.swift
+//  Torrenter
+//
+//  Created by Brian Dashore on 7/1/22.
+//
+
+import SwiftUI
+
+@main
+struct TorrenterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
