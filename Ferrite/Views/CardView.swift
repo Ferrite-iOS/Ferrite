05d61e440a3a35f2e55df3f7c3dfc6e97f74f486
kingbri <bdashore3@gmail.com>
2022-07-26 21:50:09 -0400

    Ferrite: Remove commented code
    Signed-off-by: kingbri <bdashore3@gmail.com>

--- Ferrite/Views/CardView.swift
+++ Ferrite/Views/CardView.swift
@@ -14,11 +14,3 @@ struct CardView: View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
-
-/*
- struct CardView_Previews: PreviewProvider {
- static var previews: some View {
- CardView()
- }
- }
- */
